4f285b55e0f320c62f7161f0e266ffd99de150f9
gc <dev.gauravchauhan@gmail.com>
2024-11-19 10:41:23 +0530

    initial commit

--- composeApp/src/commonMain/kotlin/com/kmm/books/book/presentation/book_list/BookListScreen.kt
+++ composeApp/src/commonMain/kotlin/com/kmm/books/book/presentation/book_list/BookListScreen.kt
@@ -1,6 +1,11 @@
 package com.kmm.books.book.presentation.book_list
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.kmm.books.book.domain.Book
+import org.koin.compose.viewmodel.koinViewModel
 
 
 //
@@ -9,6 +14,34 @@ import androidx.compose.runtime.Composable
 //
 
 @Composable
-fun BookListScreen(){
+fun BookListScreenRoot(
+    viewModel: BookListViewModel = koinViewModel(),
+    onBookCLick: (Book) -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    BookListScreen(
+        state = state,
+        onAction = { action ->
+            when (action) {
+                is BookListAction.OnBookClick -> onBookCLick(action.book)
+                else -> Unit
+            }
+            viewModel.onAction(action)
+
+        }
+
+    )
+
+}
+
+
+@Composable
+fun BookListScreen(
+    state: BookListState,
+    onAction: (BookListAction) -> Unit,
+) {
 
 }

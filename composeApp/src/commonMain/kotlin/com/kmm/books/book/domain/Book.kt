d4ae499d0347cc633b9f8b10a3ce429aa104ee8a
gc <dev.gauravchauhan@gmail.com>
2024-11-19 10:29:46 +0530

    initial commit

---
+++ composeApp/src/commonMain/kotlin/com/kmm/books/book/domain/Book.kt
@@ -0,0 +1,21 @@
+package com.kmm.books.book.domain
+
+
+//
+// Created by Code For Android on 19/11/24.
+// Copyright (c) 2024 CFA. All rights reserved.
+//
+
+data class Book(
+    val id : String,
+    val title : String,
+    val authors : List<String>,
+    val description : String,
+    val imageUrl : String,
+    val languages : List<String>,
+    val firstPublishYear : String?,
+    val averageRating : Double?,
+    val ratingsCount : Int?,
+    val numPages : Int?,
+    val numEditions : Int
+)
---
+++ composeApp/src/commonMain/kotlin/com/kmm/books/book/presentation/book_list/BookListState.kt
@@ -0,0 +1,19 @@
+package com.kmm.books.book.presentation.book_list
+
+import com.kmm.books.book.domain.Book
+import com.kmm.books.core.presentation.UiText
+
+
+//
+// Created by Code For Android on 19/11/24.
+// Copyright (c) 2024 CFA. All rights reserved.
+//
+
+data class BookListState(
+    val searchQuery: String = "Kotlin",
+    val searchResult: List<Book> = emptyList(),
+    val favorites: List<Book> = emptyList(),
+    val isLoading: Boolean = false,
+    val selectedTabIndex: Int = 0,
+    val errorMessage: UiText? = null
+)

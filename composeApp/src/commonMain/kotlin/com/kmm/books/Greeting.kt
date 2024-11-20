a2d8891c79262dd03f25fdb2ef701fb22a9a1ee9
gc <dev.gauravchauhan@gmail.com>
2024-11-20 10:30:41 +0530

    initial commit

--- composeApp/src/commonMain/kotlin/com/kmm/books/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.kmm.books
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/com/kmm/books/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.kmm.books
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform

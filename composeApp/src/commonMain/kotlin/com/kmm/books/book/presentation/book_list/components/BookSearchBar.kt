e4873f13890c59df8f5ed13d9cf59fa2742e52c6
gc <dev.gauravchauhan@gmail.com>
2024-11-20 10:04:38 +0530

    initial commit

--- composeApp/src/commonMain/kotlin/com/kmm/books/book/presentation/book_list/components/BookSearchBar.kt
+++ composeApp/src/commonMain/kotlin/com/kmm/books/book/presentation/book_list/components/BookSearchBar.kt
@@ -1,20 +1,35 @@
 package com.kmm.books.book.presentation.book_list.components
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.InputTransformation.Companion.keyboardOptions
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
+import androidx.compose.foundation.text.selection.TextSelectionColors
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExposedDropdownMenuDefaults.outlinedTextFieldColors
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import com.kmm.books.core.presentation.DarkBlue
+import com.kmm.books.core.presentation.DesertWhite
 import com.kmm.books.core.presentation.SandYellow
 import gcbookscmp.composeapp.generated.resources.Res
+import gcbookscmp.composeapp.generated.resources.clear_search
 import gcbookscmp.composeapp.generated.resources.search_hint
 import org.jetbrains.compose.resources.stringResource
 
@@ -30,35 +45,70 @@ fun BookSearchBar(
     onSearchQueryChange: (String) -> Unit,
     onImeSearch: () -> Unit,
     modifier: Modifier = Modifier
-){
+) {
+    CompositionLocalProvider(
+        LocalTextSelectionColors provides TextSelectionColors(
+            handleColor = SandYellow,
+            backgroundColor = SandYellow
+        )
+    ) {
+        OutlinedTextField(
+            value = searchQuery,
+            onValueChange = onSearchQueryChange,
+            maxLines = 1,
+            singleLine = true,
+            shape = RoundedCornerShape(100),
+            colors = OutlinedTextFieldDefaults.colors(
+                cursorColor = DarkBlue,
+                focusedBorderColor = SandYellow
 
-    OutlinedTextField(
-        value = searchQuery,
-        onValueChange = onSearchQueryChange,
-        maxLines = 1,
-        singleLine = true,
-        shape = RoundedCornerShape(100),
-        colors = OutlinedTextFieldDefaults.colors(
-            cursorColor = DarkBlue,
-            focusedBorderColor = SandYellow
+            ),
+            placeholder = {
+                Text(
+                    text = stringResource(Res.string.search_hint)
+                )
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.66f)
+                )
+            },
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    onImeSearch()
+                }
+            ),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Search
+            ),
+            trailingIcon = {
+                AnimatedVisibility(
+                    visible = searchQuery.isNotBlank()
+                ) {
+                    IconButton(
+                        onClick = {
+                            onSearchQueryChange("")
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Close,
+                            contentDescription = stringResource(Res.string.clear_search),
+                            tint = MaterialTheme.colorScheme.onSurface
+                        )
+                    }
 
-        ),
-        placeholder = {
-            Text(
-                text = stringResource(Res.string.search_hint)
-            )
-        },
-        leadingIcon = {
-            Icon(
-                imageVector = Icons.Default.Search,
-                contentDescription = null,
-                tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.66f)
-            )
-        }
+                }
+            },
+            modifier = modifier.background(
+                shape = RoundedCornerShape(100),
+                color = DesertWhite
+            ).minimumInteractiveComponentSize()
 
 
-    )
-
-
+        )
+    }
 
 }

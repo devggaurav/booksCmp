f502fd433dfab128041563e387709c279e9be7b6
gc <dev.gauravchauhan@gmail.com>
2024-11-21 10:53:22 +0530

    adding book list item

--- composeApp/src/androidMain/kotlin/com/kmm/books/SeePreviews.kt
+++ composeApp/src/androidMain/kotlin/com/kmm/books/SeePreviews.kt
@@ -8,6 +8,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.kmm.books.book.domain.Book
+import com.kmm.books.book.presentation.book_list.BookListScreen
+import com.kmm.books.book.presentation.book_list.BookListState
 import com.kmm.books.book.presentation.book_list.components.BookSearchBar
 
 
@@ -34,4 +37,36 @@ private fun BookSearchBarPreview() {
 
     }
 
+}
+
+private val books = (1..100).map {
+    Book(
+        id = it.toString(),
+        title = "Book $it",
+        imageUrl = "https://abs",
+        authors = listOf("Author $it"),
+        averageRating = 4.5678,
+        description = "Description $it",
+        languages = emptyList(),
+        firstPublishYear = null,
+        ratingsCount = 5,
+        numPages = 100,
+        numEditions = 3
+    )
+
+
+}
+
+
+@Preview
+@Composable
+private fun BookListScreenPreview() {
+
+    BookListScreen(
+        state = BookListState(
+           searchResult = books
+        )
+    ) {
+
+    }
 }
--- composeApp/src/commonMain/kotlin/com/kmm/books/book/presentation/book_list/BookListScreen.kt
+++ composeApp/src/commonMain/kotlin/com/kmm/books/book/presentation/book_list/BookListScreen.kt
@@ -7,6 +7,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -17,6 +22,8 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.kmm.books.book.domain.Book
 import com.kmm.books.book.presentation.book_list.components.BookSearchBar
 import com.kmm.books.core.presentation.DarkBlue
+import com.kmm.books.core.presentation.DesertWhite
+import com.kmm.books.core.presentation.SandYellow
 import org.koin.compose.viewmodel.koinViewModel
 
 
@@ -51,7 +58,7 @@ fun BookListScreenRoot(
 
 
 @Composable
-private fun BookListScreen(
+fun BookListScreen(
     state: BookListState,
     onAction: (BookListAction) -> Unit,
 ) {
@@ -74,6 +81,42 @@ private fun BookListScreen(
             },
             modifier = Modifier.widthIn(min = 400.dp).fillMaxWidth().padding(16.dp)
         )
+
+        Surface(
+            modifier = Modifier.weight(1f).fillMaxWidth(),
+            color = DesertWhite,
+            shape = RoundedCornerShape(topStart = 32.dp, topEnd = 32.dp)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                TabRow(
+                    selectedTabIndex = state.selectedTabIndex,
+                    modifier = Modifier.padding(vertical = 12.dp).widthIn(max = 700.dp)
+                        .fillMaxWidth()
+                ) {
+                    Tab(
+                        selected = state.selectedTabIndex == 0,
+                        onClick = {
+                            onAction(BookListAction.OnTabSelected(0))
+                        },
+                        text = {
+                            Text(text = "All")
+
+                        },
+                        modifier = Modifier.weight(1f),
+                        selectedContentColor = SandYellow,
+                        unselectedContentColor = DarkBlue.copy(alpha = 0.5f)
+
+                    )
+
+
+                }
+
+
+            }
+
+        }
 
     }
 

b24675f08704e95a21fdc4f32efe8c732a22f254
gc <dev.gauravchauhan@gmail.com>
2024-11-21 10:03:50 +0530

    adding book list item

--- composeApp/src/commonMain/kotlin/com/kmm/books/book/presentation/book_list/components/BookListItem.kt
+++ composeApp/src/commonMain/kotlin/com/kmm/books/book/presentation/book_list/components/BookListItem.kt
@@ -1,6 +1,7 @@
 package com.kmm.books.book.presentation.book_list.components
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,8 +10,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.unit.dp
+import coil3.compose.rememberAsyncImagePainter
 import com.kmm.books.book.domain.Book
 import com.kmm.books.core.presentation.LightBlue
 
@@ -35,6 +43,35 @@ fun BookListItem(
         Row(
             modifier = Modifier.padding(16.dp).fillMaxWidth().height(IntrinsicSize.Min)
         ) {
+
+            Box(
+                modifier = Modifier.height(100.dp),
+                contentAlignment = Alignment.Center
+
+            ) {
+                var imageLoadResult by remember {
+                    mutableStateOf<Result<Painter>?>(null)
+                }
+
+                val painter = rememberAsyncImagePainter(
+                    model = book.imageUrl,
+                    onSuccess = {
+                        imageLoadResult =
+                            if (it.painter.intrinsicSize.width > 1 && it.painter.intrinsicSize.height > 1) {
+                                Result.success(it.painter)
+                            } else {
+                                Result.failure(Exception("Invalid image size"))
+                            }
+                    },
+                    onError = {
+                        it.result.throwable.printStackTrace()
+                        imageLoadResult = Result.failure(it.result.throwable)
+                    }
+
+                )
+
+
+            }
 
 
         }

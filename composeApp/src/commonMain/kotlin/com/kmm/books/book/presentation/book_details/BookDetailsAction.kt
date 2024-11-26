aacbfc799d41811674beed79ef2f64040843fa31
gc <dev.gauravchauhan@gmail.com>
2024-11-26 09:52:03 +0530

    added details screen

---
+++ composeApp/src/commonMain/kotlin/com/kmm/books/book/presentation/book_details/BookDetailsAction.kt
@@ -0,0 +1,10 @@
+package com.kmm.books.book.presentation.book_details
+
+
+//
+// Created by Code For Android on 26/11/24.
+// Copyright (c) 2024 CFA. All rights reserved.
+//
+
+sealed interface BookDetailsAction {
+}

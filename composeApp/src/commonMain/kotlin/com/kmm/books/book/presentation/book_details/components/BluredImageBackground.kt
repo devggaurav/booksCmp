3357ca1d6d8a60a2f51c7196ffbce7e13062b16e
gc <dev.gauravchauhan@gmail.com>
2024-11-26 10:10:03 +0530

    added details screen

--- composeApp/src/commonMain/kotlin/com/kmm/books/book/presentation/book_details/components/BluredImageBackground.kt
+++ composeApp/src/commonMain/kotlin/com/kmm/books/book/presentation/book_details/components/BluredImageBackground.kt
@@ -1,7 +1,13 @@
 package com.kmm.books.book.presentation.book_details.components
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import coil3.compose.rememberAsyncImagePainter
 
 
 //
@@ -13,7 +19,26 @@ import androidx.compose.ui.Modifier
 fun BlurredImageBackground(
     imageUrl: String,
     isFavorite: Boolean,
+    onFavoriteClick: () -> Unit,
+    onBackClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+
+    var imageLoadResult by remember {
+        mutableStateOf<Result<Painter>?>(null)
+    }
+
+    val painter = rememberAsyncImagePainter(
+        model = imageUrl,
+        onSuccess = {
+            val size = it.painter.intrinsicSize
+            imageLoadResult = if (size.width > 1 && size.height > 1) {
+                Result.success(it.painter)
+            } else {
+                Result.failure(Exception("Invalid image size"))
+
+            }
+        },
+    )
 
 }

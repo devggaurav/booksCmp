fcd0ae1d3d6d148047f273c3b9abb1b88f870d43
gc <dev.gauravchauhan@gmail.com>
2024-11-26 10:00:44 +0530

    added details screen

---
+++ composeApp/src/commonMain/kotlin/com/kmm/books/book/presentation/book_details/components/TitledContent.kt
@@ -0,0 +1,15 @@
+package com.kmm.books.book.presentation.book_details.components
+
+import androidx.compose.runtime.Composable
+
+
+//
+// Created by Code For Android on 26/11/24.
+// Copyright (c) 2024 CFA. All rights reserved.
+//
+
+
+@Composable
+fun TitledContent() {
+
+}
